6bb19fda387c8a2a8b7223913d20e581655556d7
Eugene <71292852+EvgenBY@users.noreply.github.com>
2024-02-05 00:19:31 +0300

    Добавьте файлы проекта.

---
+++ Home_work 4/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace Home_work_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            //Exercise 1
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if(i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Exercise 2
+            //for (int i = 5; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Exercise 3
+            //Console.Write("Введи любое положительное, целое число:");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //int result = 0;
+
+            //for (int i = 1; i <= userInput; i++)
+            //{
+            //    result += i;
+            //}
+            //Console.WriteLine($"Результат сложения чисел от 1 до {userInput}  равно {result}"); ;
+
+
+            //Exercise 4
+            //int result = 0;
+            //int number = 7;
+            //int i = 1;
+            //while (result < 98)
+            //{
+            //    result = number * i++;
+            //    Console.WriteLine(result);
+
+            //}
+
+
+            //Exercise 5
+            //int[] array1 = new int[5] { 2, 22, 30, 1, 6 };
+            //int[] array2 = new int[5] { 11, 21, 3, 5, 120 };
+
+            //int arithmeticMean1 = 0;
+            //int arithmeticMean2 = 0;
+
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    arithmeticMean1 += array1[i];
+            //}
+
+            //for (int i = 0; i < array2.Length; i++)
+            //{
+            //    arithmeticMean2 += array2[i];
+            //}
+
+            //if (arithmeticMean1 > arithmeticMean2)
+            //{
+            //    Console.WriteLine("Среднее арифмитеческое значение первого массива больше второго.");
+            //}
+            //else if (arithmeticMean1 < arithmeticMean2)
+            //{
+            //    Console.WriteLine("Среднее арифмитеческое значение второго массива больше первого.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Среднее арифмитическое значение обоих массиво равно.");
+            //}
+
+
+            //Exercise *
+        }
+    }
+}
